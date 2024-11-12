313467e8f7642270b63abb82c6f922814b0b37ea
NastasyaAgps <anastasya.antipowa@gmail.com>
2024-11-12 11:15:12 +0300

    Update task_1.py

--- ЛР1/task_1.py
+++ ЛР1/task_1.py
@@ -1,14 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-summa = 0
-k = 0
-for i in numbers:
-    k += 1
-    if i is not None:
-        summa += i
-
+valid_numbers = numbers[:4] + numbers[5:]
+summa = sum(valid_numbers)
+k = len(numbers)
 sr = summa/k
-
-for a in range(len(numbers)):
-    if numbers[a] is None:
-        numbers[a] = sr
+numbers = numbers[:4] + [sr] + numbers[5:]
 print("Измененный список:", numbers)
+
